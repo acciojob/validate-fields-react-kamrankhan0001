dc3647dd68b7abbb34b4c7741fde0e0ee355035c
kamrankhan0001 <kamranbgp15@gmail.com>
2023-07-26 00:40:01 +0530

    submitting code-Wed Jul 26 2023 00:39:59 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,73 @@
 
-import React from "react";
+// import React from "react";
+ 
+
+// const App = () => {
+//   return (
+//     <div>
+//         {/* Do not remove the main div */}
+//     </div>
+//   )
+// }
+
+// export default App
+import React, { useState } from 'react';
 import './../styles/App.css';
 
-const App = () => {
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!username || !password) {
+      setErrorMessage('Please enter both username and password.');
+    } else {
+      // Your login logic here
+      setErrorMessage('');
+    }
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h2>Login Form</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+        {errorMessage && (
+          <p id="errorMessage" style={{ color: 'red' }}>
+            {errorMessage}
+          </p>
+        )}
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default LoginForm;
